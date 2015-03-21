f128fbd3b615c9a4d50c368ec65802a7c7c251f4
lmc2179 <louiscialdella@gmail.com>
2015-03-21 14:05:27 -0400

    made Runs stateless

--- analyze.py
+++ analyze.py
@@ -1,7 +1,5 @@
 from sklearn import ensemble, linear_model, cross_validation, svm, naive_bayes, neighbors, tree
 import pandas as pd
-import numpy as np
-import copy
 import constants
 
 def get_training_vectors():
@@ -16,16 +14,12 @@ def get_testing_vectors():
     feature_columns= file_contents.columns
     return file_contents[feature_columns]
 
-class AbstractRun(object): #TODO: Make stateless, model is sent into single exposed run() operation
-    def __init__(self, model):
-        self.model = model
-
-class CrossValidationRun(AbstractRun):
-    def run_cross_validation(self):
+class CrossValidationRun(object):
+    def run_cross_validation(self, model):
         training_input, training_output = get_training_vectors()
         cv_train_in, cv_test_in, cv_train_out, cv_test_out = cross_validation.train_test_split(training_input, training_output, test_size=0.7, random_state=0)
-        self.model.fit(cv_train_in, cv_train_out)
-        predictions = self.model.predict(cv_test_in)
+        model.fit(cv_train_in, cv_train_out)
+        predictions = model.predict(cv_test_in)
         correct = sum([1 for predicted, gold in zip(predictions,cv_test_out) if predicted == gold])
         percent_correct = (1.0*correct/len(predictions)) * 100
         print('Percent correct is {0}%'.format(percent_correct))
@@ -38,21 +32,21 @@ class CrossValidationRun(AbstractRun):
             buckets[key].append(element)
         return buckets
 
-class CompetitionRun(AbstractRun):
-    def train(self):
+class CompetitionRun(object):
+    def train(self, model):
         training_input, training_output = get_training_vectors()
-        self._fit_model(training_input, training_output)
+        self._fit_model(training_input, training_output, model)
 
-    def _fit_model(self, training_input, training_output):
-        self.model.fit(training_input, training_output)
+    def _fit_model(self, training_input, training_output, model):
+        model.fit(training_input, training_output)
 
-    def test(self):
+    def test(self, model):
         testing_input = get_testing_vectors()
-        predicted_output = self._predict_model(testing_input)
+        predicted_output = self._predict_model(testing_input, model)
         self._write_output_to_file(testing_input.index, predicted_output)
 
-    def _predict_model(self, testing_input):
-        return self.model.predict(testing_input)
+    def _predict_model(self, testing_input, model):
+        return model.predict(testing_input)
 
     def _write_output_to_file(self, index, predicted_output):
         f_out = open(constants.output_filename, 'w')
@@ -75,8 +69,8 @@ if __name__ == '__main__':
     model = linear_model.SGDClassifier(penalty='l1')
     # model = neighbors.KNeighborsClassifier(weights='distance')
 
-    # cv = CrossValidationRun(model)
-    # cv.run_cross_validation()
-    run = CompetitionRun(model)
-    run.train()
-    run.test()
+    cv = CrossValidationRun()
+    cv.run_cross_validation(model)
+    # run = CompetitionRun()
+    # run.train(model)
+    # run.test(model)
